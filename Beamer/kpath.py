5369c705301e8ad6c30b2d00af1664b2b970639f
Otto Kohulák <kohulak@fmph.uniba.sk>
2017-03-29 12:00:38 +0100

    Added introduction to ASE

---
+++ Beamer/kpath.py
@@ -0,0 +1,15 @@
+from   ase.io import read
+from   ase.dft.kpoints  import ibz_points
+from   ase.dft.kpoints  import get_bandpath
+
+xtal = read("POSCAR")
+
+ip = ibz_points["cubic"]
+points = [ "Gamma", "X", "M", "Gamma"]
+path = [ip[p] for p in points]
+print(xtal.cell)
+kpts, x, X = get_bandpath(path, xtal.cell, npoints = 200)
+
+print(kpts)
+print(x)
+print(X)
