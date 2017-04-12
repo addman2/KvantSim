398c5f7c831ecfc003a982481f13829f126006fd
Otto Kohulák <kohulak@fmph.uniba.sk>
2017-04-12 11:35:39 +0100

    Modified kpath selection ASE example, now it uses read_espresso_in. Also modifies sequence of frame in beamer Introduction_ASE.tex

--- Beamer/kpath.py
+++ Beamer/kpath.py
@@ -1,8 +1,8 @@
-from   ase.io import read
+from   ase.io.espresso  import read_espresso_in
 from   ase.dft.kpoints  import ibz_points
 from   ase.dft.kpoints  import get_bandpath
 
-xtal = read("POSCAR")
+xtal = read_espresso_in("x.scf.in")
 
 ip = ibz_points["cubic"]
 points = [ "Gamma", "X", "M", "Gamma"]
