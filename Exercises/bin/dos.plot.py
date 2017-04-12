a7b8a967bd689af5fa1b6af028062c9254d983d5
Otto Kohulák <kohulak@fmph.uniba.sk>
2017-04-12 10:50:50 +0100

    Added sdos plotter and modified dos plotter so now it can handle farmi energy

--- Exercises/bin/dos.plot.py
+++ Exercises/bin/dos.plot.py
@@ -7,4 +7,15 @@ from matplotlib import pyplot as plt
 E, N = np.loadtxt(sys.argv[1], usecols = (0,1), unpack = True)
 
 plt.plot(E, N)
+
+try:
+    plt.plot([float(sys.argv[2])] * 2, [max(N),0.0],"r-")
+except:
+    pass
+
+plt.xlim([min(E),max(E)])
+plt.ylim([0.0,max(N)])
+
+plt.plot(plt.xlim(),[0.0]*2,"k-")
+
 plt.savefig(sys.argv[1].replace("dos","png"))
